1ece5051f660d8b6281439566ba973818d8d754d
last_of_us <53309183+lastofussuper@users.noreply.github.com>
2025-12-23 11:50:06 +0100

    Add usage comments to extract_item_urls.js
    Added usage instructions for the browser console script.

--- scripts/extract_item_urls.js
+++ scripts/extract_item_urls.js
@@ -1,1 +1,11 @@
+/**
+* extract_item_urls.js
+*
+*Browser console script
+*usage:
+*1.open second-hand platform hidden items page (username password login required)
+*2.scroll to load all items
+*3.Open devtools console (right click-->inspect--> and u will see console on edge browser)
+*use below scripts
+
 
